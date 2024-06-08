b18c3ea0b66cc68a331b8e4806e07d416837375d
axie10 <cryptosushi21@gmail.com>
2024-06-08 11:27:57 +0200

    Manejo de errores con PANIC y RECOVER

--- ControldeErrores/main.go
+++ ControldeErrores/main.go
@@ -3,18 +3,36 @@ package main
 import (
 	// "errors"
 	"fmt"
-	"os"
+	// "os"
 	// "fmt"
 	// "strconv"
 )
 
 //funcion con manejo de errores personalizado
-func divide(diviendo, divisor int)(int, error){
-	if divisor == 0 {
-		return 0, fmt.Errorf("No se puede dividir entre 0")
-	}
-	return diviendo / divisor, nil
+// func divide(diviendo, divisor int)(int, error){
+// 	if divisor == 0 {
+// 		return 0, fmt.Errorf("No se puede dividir entre 0")
+// 	}
+// 	return diviendo / divisor, nil
+// }
+
+//funcion con PANIC
+func divide(diviendo, divisor int){
+	//funxion anonima
+	defer func (){
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	// validateZero(divisor)
+	fmt.Println(diviendo/divisor)
 }
+
+// func validateZero(divisor int){
+// 	if divisor == 0{
+// 		panic("No se puede dividir por cero")
+// 	}
+// }
 
 
 
@@ -63,13 +81,17 @@ cuando se pone en una funcion lo que se aplica es que se ejecuta al final de tod
 	// 	return
 	// }
 
-/*PANIC & RECOVER
-se utiliza para manejar situaciones excepcionales o manejar errores graves*/
+/*PANIC & RECOVER se reserva para casos excepcionales para el manejo de errores normales es con el err que devuelven las funciones file, err := func(){}
+se utiliza para manejar situaciones excepcionales o manejar errores graves
+RECOVER sirve para controlar el panic
+No hace falta hacer la panic manual, como con la funcion validatezero para usar el recover, pero nos sacara otro texto en el error, sin embargo si manejamos el panic nos sacara el texto del error que nosotros queremos*/
 
+	//al utilizar el defer hacemos que se ejcute todo y por ultimo nos saque el error y con el recover capturamos el error y lo manejamos de mejor manera captura el panic y solo no saca la frase del error, sin embargo si dejamos solo el panic nos sacara tambien en las lineas en las que se esta produciendo el error
+	divide(100,10)
+	divide(100,20)
+	divide(100,0)
+	divide(100,300)
 	
-
-
-
 
 
 }
